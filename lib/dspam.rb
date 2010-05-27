8b43fa683a845694d2c3942a2e3ef46946f0f983
Florian G. Pflug <fgp@phlo.org>
2010-05-27 17:50:32 +0000

    Ensure that we reset Thread::critical
    git-svn-id: svn+ssh://syn.phlo.org/svn/fgp.svn/private/master.phlo.org/imaputils@1379 8c8cc1e4-acd0-0310-b9ee-f216a4ea5b73

--- lib/dspam.rb
+++ lib/dspam.rb
@@ -4,25 +4,50 @@ module DSPAM
   class DSPAMError < Exception
   end
 
+  # dspam(input, arg1, arg2, ...): Executes the dspam binary with command line
+  #                                arguments <arg1>, <arg2>, ... and feeds it the
+  #                                string <input> on it's stdin.
+  #                                Raises a DSPAMError if the binary exist with
+  #                                a non-zero exit code.
   def self.dspam(input, *args)
+    # Create the pipes used as stdin,stdout and stderr of the child process
     in_read, in_write = *IO::pipe
     out_read, out_write = *IO::pipe
  
+    # Redudant, but improved the error message for the very common case of a wrong path.
     raise DSPAMError::new("Couldn't find dspam executable: #{SXCfg::Default.dspam.command.string}") unless
       FileTest::executable? SXCfg::Default.dspam.command.string
 
-    Thread::critical = true
-    if (pid = fork).nil? then
-      in_write.close; out_read.close
+    # fork() forks the whole interpreted, including *all* running threads. We do *not* want any of them
+    # to be scheduled in the child, since they probably won't deal with that gracefully.
+    begin
+      Thread::critical = true
+      if (pid = fork).nil? then
+        # Running inside the child process now. Close the parent's ends of
+        # the pipes, and redirect all IO to in_write and out_read before
+        # executing the DSPAM binary.
 
-      STDIN.reopen(in_read)
-      STDOUT.reopen(out_write); STDERR.reopen(out_write)
-      in_read.close
-      out_write.close
+        in_write.close; out_read.close
 
-      Kernel::exec(SXCfg::Default.dspam.command.string, *args)
+        STDIN.reopen(in_read)
+        STDOUT.reopen(out_write); STDERR.reopen(out_write)
+        in_read.close
+        out_write.close
+
+        Kernel::exec(SXCfg::Default.dspam.command.string, *args)
+      
+        # If exec is successfull, it does not return. Instead, the process
+        # calling exec gets *replaced* with the executed binary. Hence,
+        # if we get here, an error occured.
+        exit(1)
+      end
+    ensure
+      Thread::critical = false
     end
 
+    # Parent process continues here. Close the child's ends of the pipes,
+    # and feed <input> to the child before reading it's output and then
+    # waiting for it to exit.
     in_read.close; out_write.close
     in_write.write input if input
     in_write.close
@@ -30,23 +55,30 @@ module DSPAM
     out_read.close
     dummy, status = *Process::waitpid2(pid)
 
+    # Complain if DSPAM returned a non-zero exit code    
     raise DSPAMError::new("#{SXCfg::Default.dspam.command.string} #{args.join(' ')}\n" + output) unless status == 0
-  ensure
-    Thread::critical = false
   end
    
+  # miss_junk(user, sig): Make DSPAM relearn the message with signature <sig> as junk for user <user>
   def self.miss_junk(user, sig)
     dspam(nil, "--client", "--user", user, "--signature=#{sig}", "--source=error", "--class=spam")
   end
 
+  # miss_junk(user, sig): Make DSPAM relearn the message with signature <sig> as innocent for user <user>
   def self.miss_innocent(user, sig)
     dspam(nil, "--client", "--user", user, "--signature=#{sig}", "--source=error", "--class=innocent")
   end
 
+  # miss_junk(user, sig): Feed the junk message <raw> to DSPAM for training purposes. Note *do* *not*
+  #                       use this for messages which DSPAM has incorrectly classified as innocent!
+  #                       Use miss_junk for that! This method is for training dspam based on a corpus only!
   def self.corpus_junk(user, raw)
     dspam(raw, "--client", "--user", user, "--source=corpus", "--class=spam")
   end
 
+  # miss_innocent(user, sig): Feed the innocent message <raw> to DSPAM for training purposes. Note *do* *not*
+  #                           use this for messages which DSPAM has incorrectly classified as junk!
+  #                           Use miss_innocent for that! This method is for training dspam based on a corpus only!
   def self.corpus_innocent(user, raw)
     dspam(raw, "--client", "--user", user, "--source=corpus", "--class=innocent")
   end
